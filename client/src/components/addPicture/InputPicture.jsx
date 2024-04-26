5e294afc28b730b8f672cd2b7ccc742d5ff3f771
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-04-26 20:52:47 +0200

    creation du composant InputPicture et ajout des tests

---
+++ client/src/components/addPicture/InputPicture.jsx
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types";
+import { useDragNDrop } from "../../utils/hooks/useDragNDrop";
+import { icons } from "../../assets/icons/icons";
+
+export const InputPicture = ({ onInputChange, inputRef }) => {
+  const { dragging, dragOver, dragEnter, dragLeave, fileDrop } = useDragNDrop();
+  return (
+    <div
+    data-testid="inputPicture"
+      className={`add-picture__wrapper ${dragging ? "add-picture__wrapper--dragging" : ""}`}
+      onDragOver={dragOver}
+      onDragEnter={dragEnter}
+      onDragLeave={dragLeave}
+      onDrop={(e) => {
+        const files = fileDrop(e);
+        if (files.length) {
+          onInputChange({ target: { files } });
+        }
+      }}
+    >
+      <span className="add-picture__icon" role="icon">{icons.image}</span>
+      <label htmlFor="picture" className="add-picture__wrapper add-picture__btn" role="button">{icons.plusCircle}</label>
+      <input
+          type="file"
+          name="picture"
+          id="picture"
+          className="add-picture__input"
+          onChange={onInputChange}
+          ref={inputRef}
+          role="addPictureButton"
+        />
+        <p className="add-picture__txt" role="txt">jpg, png : 15mo max</p>
+    </div>
+  );
+};
+
+InputPicture.propTypes = {
+  onInputChange: PropTypes.func,
+    inputRef: PropTypes.object,
+};
---
+++ client/src/components/addPicture/inputPicture.test.jsx
@@ -0,0 +1,84 @@
+import { describe, test, expect, vi } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { InputPicture } from "./InputPicture";
+import { useDragNDrop } from "../../utils/hooks/useDragNDrop";
+import {act, renderHook} from "@testing-library/react";  
+
+
+// Tests d'intégration
+describe("InputPicture component", () => {
+  test("renders InputPicture component and checks DOM elements", () => {
+    const onInputChange = vi.fn();
+    render(<InputPicture onInputChange={onInputChange} />);
+
+    // vérifie si l'élément addPicture est présent dans le DOM
+    const addPicture = screen.getByRole("addPictureButton");
+    expect(addPicture).toBeInTheDocument();
+
+    // vérifie si l'élément icon est présent dans le DOM
+    const icon = screen.getByRole("icon");
+    expect(icon).toBeInTheDocument();
+
+    // verifie si l'élément btn est présent dans le DOM
+    const btn = screen.getByRole("button");
+    expect(btn).toBeInTheDocument();
+
+    // vérifie si l'élément txt est présent dans le DOM
+    const txt = screen.getByRole("txt");
+    expect(txt).toBeInTheDocument();
+  });
+});
+
+// Tests unitaires
+describe("InputPicture component", () => {
+  test("checks onInputChange function call", () => {
+    const onInputChange = vi.fn();
+    render(<InputPicture onInputChange={onInputChange} />);
+    const input = screen.getByRole("addPictureButton");
+    fireEvent.change(input);
+
+    expect(onInputChange).toHaveBeenCalledTimes(1);
+  });
+
+  test("checks onInputChange function call when a file is dropped", () => {
+    const files = [new File([], "test.png")];
+    const onInputChange = vi.fn();
+    render(<InputPicture onInputChange={onInputChange} />);
+  
+    const input = screen.getByTestId("inputPicture");
+    fireEvent.drop(input, { dataTransfer: { files } });
+  
+    expect(onInputChange).toHaveBeenCalledTimes(1);
+  });
+
+  test("checks dragOver function call", () => {
+    const { result } = renderHook(() => useDragNDrop());
+    const { dragOver } = result.current;
+    const e = { preventDefault: vi.fn() };
+    act(() => dragOver(e));
+  
+    expect(e.preventDefault).toHaveBeenCalledTimes(1);
+  });
+
+  test("checks className changes when dragging",  () => {
+    render(<InputPicture onInputChange={vi.fn()} />);
+    const e = { preventDefault: vi.fn() };  
+    const { result } = renderHook(() => useDragNDrop());
+    act(() => result.current.dragOver(e));
+
+     expect(result.current.dragging).toBe(true);
+  });
+  test("checks className changes when dragging", () => {
+    render(<InputPicture onInputChange={vi.fn()} />);
+    fireEvent(
+      screen.getByTestId("inputPicture"),
+      new MouseEvent("dragover", {
+        bubbles: true,
+        cancelable: true,
+      })
+    )
+    
+    const inputPicture = screen.getByTestId("inputPicture");
+    expect(inputPicture).toHaveClass("add-picture__wrapper--dragging");
+  });
+});

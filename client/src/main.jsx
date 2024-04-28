3cd1b7306e91e93f0c243e9ac93d389925304d6f
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-04-28 14:29:27 +0200

    Mise à jour du composant Main pour supprimer l'import inutile et ajouter le composant App dans le rendu

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./scss/index.css";
-import { Provider } from "react-redux";
-import { store } from "./app/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
 );

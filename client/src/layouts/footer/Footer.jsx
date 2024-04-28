0b72f79cb2756eddf8e14c052af4f15c38052a8a
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-04-28 14:38:38 +0200

    Mise à jour du lien vers le code source dans le composant Footer.jsx

--- client/src/layouts/footer/Footer.jsx
+++ client/src/layouts/footer/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <footer className="footer" data-testid="footer">
-            <p>Code source available on github</p>
+            <p>Code source available on <a href="https://github.com/Ax-07/picture-convert" target="blank"><strong>Github</strong></a></p>
         </footer>
     );
 };

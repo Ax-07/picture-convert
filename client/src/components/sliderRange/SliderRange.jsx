8434abb3eae423c6bcad74e77dd6b99483488fb3
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-04-28 12:41:02 +0200

    Mise à jour du composant SliderRange pour utiliser le rôle "spinbutton" pour l'input au lieu de "slider"

--- client/src/components/sliderRange/SliderRange.jsx
+++ client/src/components/sliderRange/SliderRange.jsx
@@ -1,29 +1,46 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export const SliderRange = ({min, max, value, setValue}) => {
-    return (
-        <div className='sliderRange' data-testid='sliderRange'>
-            <input data-testid='slider' className='sliderRange__input' type='range' min={min} max={max} value={value} step={1} onChange={(e)=> setValue(Number(e.target.value))}/>
-            <div className='sliderRange__wrapper' data-testid="wrapper">
-                <p role='paragraph'>{0}</p>
-                <p role='paragraph'>|</p>
-                <p role='paragraph'>{20}</p>
-                <p role='paragraph'>|</p>
-                <p role='paragraph'>{40}</p>
-                <p role='paragraph'>|</p>
-                <p role='paragraph'>{60}</p>
-                <p role='paragraph'>|</p>
-                <p role='paragraph'>{80}</p>
-                <p role='paragraph'>|</p>
-                <p role='paragraph'>{max}</p>
-            </div>
-        </div>
-    );
+export const SliderRange = ({ min, max, value, setValue }) => {
+  return (
+    <div className="sliderRange" data-testid="sliderRange">
+      <input
+        type="number"
+        onChange={(e) => setValue(Number(e.target.value))}
+        id="quality"
+        min={1}
+        max={100}
+        value={value}
+      />
+      <input
+        data-testid="slider"
+        className="sliderRange__input"
+        type="range"
+        min={min}
+        max={max}
+        value={value}
+        step={1}
+        onChange={(e) => setValue(Number(e.target.value))}
+      />
+      <div className="sliderRange__wrapper" data-testid="wrapper">
+        <p role="paragraph">{0}</p>
+        <p role="paragraph">|</p>
+        <p role="paragraph">{20}</p>
+        <p role="paragraph">|</p>
+        <p role="paragraph">{40}</p>
+        <p role="paragraph">|</p>
+        <p role="paragraph">{60}</p>
+        <p role="paragraph">|</p>
+        <p role="paragraph">{80}</p>
+        <p role="paragraph">|</p>
+        <p role="paragraph">{max}</p>
+      </div>
+    </div>
+  );
 };
 
 SliderRange.propTypes = {
-    min: PropTypes.number.isRequired,
-    max: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-    setValue: PropTypes.func.isRequired
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+  setValue: PropTypes.func.isRequired,
 };

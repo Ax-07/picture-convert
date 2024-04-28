4cd25b2bd532621baa5f0ee270d9093f1871c50e
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-04-28 12:40:49 +0200

    Mise à jour du composant SliderRange pour utiliser le rôle "spinbutton" pour l'input au lieu de "slider"

--- client/src/components/sliderRange/index.test.jsx
+++ client/src/components/sliderRange/index.test.jsx
@@ -6,20 +6,32 @@ test('renders SliderRange component', () => {
     render(<SliderRange min={0} max={100} value={50} setValue={()=>{}} />);
     const sliderRange = screen.getByTestId('sliderRange');
     expect(sliderRange).toBeInTheDocument();
-    const input = screen.getByRole('slider');
-    expect(input).toBeInTheDocument();
+    const input1 = screen.getByRole('spinbutton');
+    expect(input1).toBeInTheDocument();
+    const input2 = screen.getByRole('slider');
+    expect(input2).toBeInTheDocument();
     const wrapper = screen.getByTestId('wrapper');
     expect(wrapper).toBeInTheDocument();
     const p = screen.getAllByRole('paragraph');
     expect(p.length).toBe(11);
 });
 
-test('when value is changed', () => {
+test('when value is changed with the slider', () => {
     let testValue = 50;
     const setValue = (value) => testValue = value;
     
     render(<SliderRange min={0} max={100} value={50} setValue={setValue} />);
     const input = screen.getByRole('slider');
+    fireEvent.change(input, { target: { value: '20' } });
+    expect(testValue).toBe(20);
+});
+
+test('when value is changed with the input', () => {
+    let testValue = 50;
+    const setValue = (value) => testValue = value;
+    
+    render(<SliderRange min={0} max={100} value={50} setValue={setValue} />);
+    const input = screen.getByRole('spinbutton');
     fireEvent.change(input, { target: { value: '20' } });
     expect(testValue).toBe(20);
 });

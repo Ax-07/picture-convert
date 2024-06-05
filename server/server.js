21b41aa73f678aac125012ff501eba8994b81e94
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-06-05 18:44:02 +0200

    maj configuration cors et delete sitemap

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express(); // Création d'une nouvelle application express
 const cors = require('cors'); // Importation du module cors pour gerer les origines
-app.use(cors()); // Configuration de l'application pour utiliser le module cors
+require('dotenv').config(); // Importation du module dotenv pour charger les variables d'environnement
+
+const corsOptions = {
+  origin: process.env.URL_ORIGIN, // Remplacez par l'origine de votre client
+  methods: ['GET', 'POST'], // Méthodes autorisées
+  credentials: true // Autorise les cookies
+};
+app.use(cors(corsOptions)); // Configuration de l'application pour utiliser le module cors
 app.use(express.json()); // Configuration de l'application pour utiliser le format JSON
 app.use(express.urlencoded({ extended: true })); // Configuration de l'analyseur de corps de requête pour analyser les requêtes en format JSON
 
 // Importation des routes
 const convertPictureRoute = require('./routes/convertPicture.routes');
 app.use('/api', convertPictureRoute); // Configuration de l'application pour utiliser les routes de convertPicture
-const siteMap = require('./routes/siteMap.routes');
-app.use(siteMap); // Configuration de l'application pour utiliser les routes de siteMap
 
 // Swagger API documentation
 const swaggerUi = require('swagger-ui-express'); // Importation du module swagger-ui-express

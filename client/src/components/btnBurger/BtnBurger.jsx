d2b28e86c67b0095b21301c213f02b20d6a11728
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-06-26 14:58:32 +0200

    mise en forme

--- client/src/components/btnBurger/BtnBurger.jsx
+++ client/src/components/btnBurger/BtnBurger.jsx
@@ -7,13 +7,13 @@ import { BtnStart } from "../btn/Buttons";
 const logo = "Picture convert";
 
 const navData = [
-    { id: 1, name: "Comment ça marche ?", link: "/#how-it-works" },
-    { id: 2, name: "A propos", link: "/#about" },
-    { id: 3, name: "Conseil", link: "/#advice"},
-    { id: 4, name: "Convertir", link: "/application/convert" },
-    { id: 5, name: "Compresser", link: "/application/compress" },
-    { id: 6, name: "Multi-Size", link: "/application/multi-size" },
-  ];
+  { id: 1, name: "Comment ça marche ?", link: "/#how-it-works" },
+  { id: 2, name: "A propos", link: "/#about" },
+  { id: 3, name: "Conseil", link: "/#advice" },
+  { id: 4, name: "Convertir", link: "/application/convert" },
+  { id: 5, name: "Compresser", link: "/application/compress" },
+  { id: 6, name: "Multi-Size", link: "/application/multi-size" },
+];
 export const BtnBurger = ({ windowWidth }) => {
   const [isOpen, setIsOpen] = useState(false);
   const scrollToTop = () => {
@@ -30,21 +30,24 @@ export const BtnBurger = ({ windowWidth }) => {
   }, [windowWidth]);
 
   return (
-    <div className={`burgerBtn ${isOpen ? "burgerBtn--open" : ""}`} onClick={toggleMenu}>
+    <div
+      className={`burgerBtn ${isOpen ? "burgerBtn--open" : ""}`}
+      onClick={toggleMenu}
+    >
       {windowWidth < 1024 ? (
         <>
-        <div className="burgerBtn__lines">
-          <span className="burgerBtn__line"></span>
-          <span className="burgerBtn__line"></span>
-          <span className="burgerBtn__line"></span>
-        </div>
- <div className="burgerBtn__menu">
- <Link to="/#" className={`navbar__link`} onClick={scrollToTop}>
-          <h1>{logo}</h1>
-        </Link>
-          <Navbar navData={navData} />
-          <BtnStart />
-        </div>
+          <div className="burgerBtn__lines">
+            <span className="burgerBtn__line"></span>
+            <span className="burgerBtn__line"></span>
+            <span className="burgerBtn__line"></span>
+          </div>
+          <div className="burgerBtn__menu">
+            <Link to="/#" className={`navbar__link`} onClick={scrollToTop}>
+              <h1>{logo}</h1>
+            </Link>
+            <Navbar navData={navData} />
+            <BtnStart />
+          </div>
         </>
       ) : null}
     </div>

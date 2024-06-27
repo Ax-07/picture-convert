0df8be2e5e70a8f18d4466b2d0d2246d971cda2f
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-06-27 10:43:46 +0200

    ajout de bouton de navigation

--- client/src/pages/app/Application.jsx
+++ client/src/pages/app/Application.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Convert } from "../app/convert/Convert";
 import { Compress } from "../app/compress/Compress";
 import { MultiSize } from "../app/multiSize/MultiSize";
@@ -5,6 +6,9 @@ import { Routes, Route } from "react-router-dom";
 import { Navbar } from "../../components/navigation/navbar/Navbar";
 import waveTop from "../../assets/svg/WaveTop.svg";
 import { SeparateLine } from "../../components/separateLines/SeparateLine";
+import refresh from "../../assets/svg/faRefresh.svg";
+import compress from "../../assets/svg/faCompressArrowsAlt.svg";
+import photoStack from '../../assets/svg/photo.stack.svg';
 
 const navData = [
     { id: 1, name: "Accueil", link: "/application" },
@@ -39,6 +43,19 @@ const DefaultMessage = () => {
     <section>
       <h1>Bienvenue !</h1>
       <p>Veuillez sélectionner une option dans le menu.</p>
+      <div className="application__list">
+      <Link to='/application/convert' className='application__btn-link'>
+        <img src={refresh} alt="icon liens convert" />
+        <h2>Convertir</h2>
+      </Link>
+      <Link to='/application/compress' className='application__btn-link'>
+        <img src={compress} alt="icon liens compress" />
+        <h2>Compresser</h2>
+      </Link>
+      <Link to='/application/multi-size' className='application__btn-link'>
+        <img src={photoStack} alt="icon liens multi-size" />
+        <h2>Multi-tailles</h2>
+      </Link></div>
     </section>
   );
 };

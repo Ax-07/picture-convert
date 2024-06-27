316b9622fe8c1c41409940707b65b578a92284cf
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-06-27 18:34:59 +0200

    ajout documentation  du context

--- client/src/context/PicturesContext.jsx
+++ client/src/context/PicturesContext.jsx
@@ -4,6 +4,31 @@ import { initPictureProperty } from "../utils/functions/blobUtils";
 
 export const PictureContext = createContext();
 
+/**
+ * @description - Un context React qui fournit les états et fonctions pour gérer les images.
+ * @param {Object} children - Les composants enfants.
+ * 
+ * @returns - Un context React qui fournit les états et fonctions pour gérer les contenant :
+ *  isDisplayPreviewPicture - Un booléen indiquant si la prévisualisation de l'image est affichée.
+ *  setIsDisplayPreviewPicture - Une fonction pour définir l'état isDisplayPreviewPicture.
+ *  previewPicture - L'image à prévisualiser.
+ *  setPreviewPicture - Une fonction pour définir l'image à prévisualiser.
+ *  reponse - La réponse de l'API.
+ *  setReponse - Une fonction pour définir la réponse de l'API.
+ *  images - Les images à traiter.
+ *  setImages - Une fonction pour définir les images à traiter.
+ *  quality - La qualité de l'image.
+ *  setQuality - Une fonction pour définir la qualité de l'image.
+ *  sizes - Les tailles des images.
+ *  setSizes - Une fonction pour définir les tailles des images.
+ *  originalPictureProperty - Les propriétés de l'image originale.
+ *  setOriginalPictureProperty - Une fonction pour définir les propriétés de l'image originale.
+ *  comparedImageProperty - Les propriétés de l'image à comparer.
+ *  setComparedImageProperty - Une fonction pour définir les propriétés de l'image à comparer.
+ *  resetPictures - Une fonction pour réinitialiser tous les états.
+ * 
+ */
+
 export const PictureProvider = ({ children }) => {
   const [isDisplayPreviewPicture, setIsDisplayPreviewPicture] = useState(false);
   const [previewPicture, setPreviewPicture] = useState(null);
